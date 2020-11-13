e6b0ac5118dc9bf9300b53bec069fde75df27416
fadyat <70910148+fadyat@users.noreply.github.com>
2020-11-13 23:12:42 +0300

    Update lab1.py

--- Linal/Labs/Solutions/lab1.py
+++ Linal/Labs/Solutions/lab1.py
@@ -1,122 +1,114 @@
-# with open("input.txt", "r") as file:
-def readMatrix(file):
-    line = file.readline()
-    an, am = map(int, line.split())
-    line = file.readline()
-    matrix = []
-    for i in range(an):
-        matrix.append([0] * am)
-    p = [float(i) for i in line.split()]
-    for i in range(an):
-        for j in range(am):
-            matrix[i][j] = p[i * am + j]
-    return matrix
+def build(FILE):
+    line = FILE.readline()
+    n, m = map(int, line.split())
+    ans = []
+    tmp = []
+    line = FILE.readline()
+    for i in line.split():
+        tmp.append(float(i))
+    for i in range(n):
+        tmp1 = []
+        for j in range(m):
+            tmp1.append(tmp[i * m + j])
+        ans.append(tmp1)
+    return ans
 
 
-def transMatrix(matrix):
-    n = len(matrix)
-    m = len(matrix[0])
-    t = []
+def transport(mtx):
+    n = len(mtx)
+    m = len(mtx[0])
+    ans = []
     for i in range(m):
-        t.append([0] * n)
+        tmp = []
+        for j in range(n):
+            tmp.append(mtx[j][i])
+        ans.append(tmp)
+    return ans
+
+
+def on_value(k, ans):
+    n = len(ans)
+    m = len(ans[0])
     for i in range(n):
         for j in range(m):
-            t[j][i] = matrix[i][j]
-    return t
+            ans[i][j] *= k
+    return ans
 
 
-def sumMatrix(m1, m2):
-    n1 = len(m1)
-    n2 = len(m2)
-    ml1 = len(m1[0])
-    print(ml1)
-    ml2 = len(m2[0])
-    if n1 != n2 or ml1 != ml2:
-        return False
-    n = n1
-    m = ml2
-    s = []
-    for i in range(n):
-        s.append([0] * m)
-    # s = [[0] * m for i in range(n)]
-    for i in range(n):
-        for j in range(m):
-            s[i][j] = m1[i][j] + m2[i][j]
-    return s
+def mtx_sum(mtx1, mtx2):
+    n1 = len(mtx1)
+    m1 = len(mtx1[0])
+    n2 = len(mtx2)
+    m2 = len(mtx2[0])
+    if n1 != n2 or m1 != m2:
+        return -1
+    for i in range(n1):
+        for j in range(m1):
+            mtx1[i][j] += mtx2[i][j]
+    return mtx1
 
 
-def valMatrixCompostion(a, matrix):
-    n = len(matrix)
-    m = len(matrix[0])
-    s = []
-    # s = [[0] * m for i in range(n)]
-    for i in range(n):
-        s.append([0] * m)
-    for i in range(n):
-        for j in range(m):
-            s[i][j] = matrix[i][j] * a
-    return s
-
-
-def matrixCompostion(matrix1, matrix2):
-    n1 = len(matrix1)
-    m1 = len(matrix1[0])
-    n2 = len(matrix2)
-    m2 = len(matrix2[0])
+def on_mtx(mtx1, mtx2):
+    n1 = len(mtx1)
+    m1 = len(mtx1[0])
+    n2 = len(mtx2)
+    m2 = len(mtx2[0])
     if m1 != n2:
         return -1
-    t = []
-    # t = [[0] * m2 for i in range(n1)]
+    ans = []
     for i in range(n1):
-        t.append([0] * m2)
-    for i in range(n1):
+        tmp = []
         for j in range(m2):
             s = 0
-            for q in range(m1):
-                s += matrix1[i][q] * matrix2[q][j]
-            t[i][j] = s
-    return t
+            for k in range(m1):
+                s += mtx1[i][k] * mtx2[k][j]
+            tmp.append(s)
+        ans.append(tmp)
+    return ans
 
 
-def func(z, x, a, b, c, d, f):
-    a = valMatrixCompostion(z, a)
-    b = transMatrix(b)
-    b = valMatrixCompostion(x, b)
-    s = transMatrix(sumMatrix(a, b))
-    if s == -1:
+def operations(k1, k2, a, b, c, d, f):
+    # ans = C·((k1·A + k2·(B)^T)^T)·D − F
+    a = on_value(k1, a)
+    b = transport(b)
+    b = on_value(k2, b)
+    new = mtx_sum(a, b)
+    if new == -1:
         return -1
-    s = matrixCompostion(c, s)
-    if s == -1:
+    new = transport(new)
+    new = on_mtx(c, new)
+    if new == -1:
         return -1
-    s = matrixCompostion(s, d)
-    if s == -1:
+    new = on_mtx(new, d)
+    if new == -1:
         return -1
-    f = valMatrixCompostion(-1, f)
-    s = sumMatrix(s, f)
-    return s
+    f = on_value(-1, f)
+    new = mtx_sum(new, f)
+    if new == -1:
+        return -1
+    return new
 
 
-with open("input.txt", "r") as file:
-    line = file.readline()
-    z, x = map(float, line.split())
-    a = readMatrix(file)
-    b = readMatrix(file)
-    c = readMatrix(file)
-    d = readMatrix(file)
-    f = readMatrix(file)
-# print(matrixCompostion([[1, 2], [1,3]],[[1,7], [5,5]]))
-with open("output.txt","w") as file:
-    q = func(z, x, a, b, c, d, f)
-    if q == -1:
-        file.write("0")
-        file.write("\n")
+# main
+with open("input.txt", "r") as FILE:
+    line = FILE.readline()
+    k1, k2 = map(float, line.split())
+    a = build(FILE)
+    b = build(FILE)
+    c = build(FILE)
+    d = build(FILE)
+    f = build(FILE)
+
+with open("output.txt", "w") as FILE:
+    _ans = operations(k1, k2, a, b, c, d, f)
+    if _ans == -1:
+        FILE.write("0\n")
     else:
-        file.write("1")
-        file.write("\n")
-        file.write(str(len(q)) +" "+ str(len(q[0])))
-        file.write("\n")
-        n = len(q)
-        m = len(q[0])
+        FILE.write("1\n")
+        n = len(_ans)
+        m = len(_ans[0])
+        FILE.write(str(n) + ' ' + str(m) + '\n')
         for i in range(n):
             for j in range(m):
-                file.write(str(float(q[i][j])) + " ")
+                FILE.write(str(_ans[i][j]) + ' ')
+            FILE.write('\n')
