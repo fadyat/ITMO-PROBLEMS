c09768e677c3b8388572a91c523f300e83ec05a4
Artyom Fadeyev <70910148+fadyat@users.noreply.github.com>
2020-11-26 12:09:31 +0300

    Update main.c

--- Programming/C/Labs/labb14/main.c
+++ Programming/C/Labs/labb14/main.c
@@ -36,9 +36,9 @@
 
 int friends(int i, int j, unsigned char **Colors, int h, int w) {
     int lives = 0;
-    for(int i1 = i - 1; i1 <= i + 1; i1++) {
-        for(int j1 = j - 1; j1 <= j + 1; j1++) {
-            if(i == i1 && j == j1) {
+    for (int i1 = i - 1; i1 <= i + 1; i1++) {
+        for (int j1 = j - 1; j1 <= j + 1; j1++) {
+            if (i == i1 && j == j1) {
                 continue;
             }
             lives += ((Colors[(i1 + h) % h][(j1 + w) % w] == '1') ? (1) : (0));
@@ -47,49 +47,49 @@ int friends(int i, int j, unsigned char **Colors, int h, int w) {
     return lives;
 }
 
-void next(int h, int w, unsigned char **Colors) {
+void next(int h, int w, int realWidth, unsigned char **Colors) {
     unsigned char new_area[h][w];
     for (int i = h - 1; i >= 0; i--) {
-        for (int j = 0; j < w; j++) {
-            int live = friends(i, j, Colors, h, w);
-            if(Colors[i][j] == '1' && !(live == 2 || live == 3)) {
+        for (int j = 0; j < realWidth; j++) {
+            int live = friends(i, j, Colors, h, realWidth);
+            if (Colors[i][j] == '1' && !(live == 2 || live == 3)) {
                 new_area[i][j] = '0';
-            }
-            else if(Colors[i][j] == '0' && live == 3) {
+            } else if (Colors[i][j] == '0' && live == 3) {
                 new_area[i][j] = '1';
-            }
-            else {
+            } else {
                 new_area[i][j] = Colors[i][j];
             }
         }
     }
 
-    for(int i = h - 1; i >= 0; i--) {
+    for (int i = h - 1; i >= 0; i--) {
         for (int j = 0; j < w; j++) {
             Colors[i][j] = new_area[i][j];
         }
     }
 }
-struct pall{
+
+struct pall {
     unsigned char R;
     unsigned char G;
     unsigned char B;
     unsigned char A;
 };
 
-void image(unsigned char **Colors, struct pall Palette[], int h, int w, char* out, unsigned char header[54], int size_Palette) {
+void image(unsigned char **Colors, struct pall Palette[],
+           int h, int w, char *out, unsigned char header[54], int PalSz) {
     FILE *output;
     output = fopen(out, "w");
-    for(int i = 0; i < 54; i++) {
+    for (int i = 0; i < 54; i++) {
         fprintf(output, "%c", header[i]);
     }
-    for(int i = 0; i < size_Palette; i++) {
+    for (int i = 0; i < PalSz; i++) {
         fprintf(output, "%c%c%c%c", Palette[i].R, Palette[i].G, Palette[i].B, Palette[i].A);
     }
-    for(int i = h - 1; i >= 0; i--) {
-        for(int j = 0; j < w; j += 8) {
+    for (int i = h - 1; i >= 0; i--) {
+        for (int j = 0; j < w; j += 8) {
             int sum = 0;
-            for(int q = 0; q < 8; q++) {
+            for (int q = 0; q < 8; q++) {
                 int tmp = (Colors[i][j + q] == '1') ? (1) : (0);
                 sum += tmp * (int) pow(2, 7 - q);
             }
@@ -100,54 +100,49 @@ void image(unsigned char **Colors, struct pall Palette[], int h, int w, char* ou
     fclose(output);
 }
 
-int main(/*int argc, char *argv[]*/) {
-    
-    
-    
-    
-    // Still in process
-    
-
-
-
-
+int main(int argc, char *argv[]) {
     FILE *my_picture;
     my_picture = fopen("picture.bmp", "r");
 
     // * Check my File
-    if(my_picture == NULL) {
+    if (my_picture == NULL) {
         printf("Not such file in directory\n");
         return 0;
     }
-
     /*
      * Take BitMapFileHeader + BitMapInfoHeader and add this info
      *  to header[sizeof(BitMapFileHeader + BitMapInfoHeader) = 54]
      */
+    for (int i = 1; i < argc; i++) {
+        if (argv[i] == "--input") {
+            printf("%s", argv[i + 1]);
+            i++;
+        }
+    }
     unsigned char header[54];
-    fread(header, 1, 54,  my_picture);
+    fread(header, 1, 54, my_picture);
     int width;
     width = (int) (header[21] * pow(256, 3) +
-            header[20] * pow(256, 2) +
-            header[19] * pow(256, 1) +
-            header[18] * pow(256, 0));
+                   header[20] * pow(256, 2) +
+                   header[19] * pow(256, 1) +
+                   header[18] * pow(256, 0));
     int height;
     height = (int) (header[25] * pow(256, 3) +
-            header[24] * pow(256, 2) +
-            header[23] * pow(256, 1) +
-            header[22] * pow(256, 0));
+                    header[24] * pow(256, 2) +
+                    header[23] * pow(256, 1) +
+                    header[22] * pow(256, 0));
     /*
      * My Image starts from 63 byte
      * Let's fill 54 - 63 bytes in Palette
      */
     int OffsetBits;
     OffsetBits = (int) (header[13] * pow(256, 3) +
-           header[12] * pow(256, 2) +
-           header[11] * pow(256, 1) +
-           header[10] * pow(256, 0));
-    
+                        header[12] * pow(256, 2) +
+                        header[11] * pow(256, 1) +
+                        header[10] * pow(256, 0));
+
     struct pall Palette[OffsetBits - 54];
-    for(int i = 0; i < (OffsetBits - 54) / 4; i++) {
+    for (int i = 0; i < (OffsetBits - 54) / 4; i++) {
         unsigned char bytes[4];
         fread(bytes, 1, 4, my_picture);
         Palette[i].R = bytes[0];
@@ -161,21 +156,22 @@ int main(/*int argc, char *argv[]*/) {
      * from the bottom to top
      */
     unsigned char **Colors;
-    Colors = (unsigned char **) malloc(height * sizeof(unsigned char*));
-    for(int i = 0; i < height; i++) {
-        Colors[i] = (unsigned char *)malloc(width * sizeof(unsigned char*));
+    Colors = (unsigned char **) malloc(height * sizeof(unsigned char *));
+    for (int i = 0; i < height; i++) {
+        Colors[i] = (unsigned char *) malloc(width * sizeof(unsigned char *));
     }
+    int realWidth = width;
     // * Width should be % 4 == 0
     width += 32 - width % 32;
     // * Filling
-    for(int i = height - 1; i >= 0; i--) {
+    for (int i = height - 1; i >= 0; i--) {
         for (int j = 0; j < width; j += 8) {
             unsigned char byte[1];
             fread(byte, 1, 1, my_picture);
             int byte_ = byte[0];
             int step = 7;
             // * Saving colors
-            while(step >= 0) {
+            while (step >= 0) {
                 Colors[i][j + step] = ((byte_ % 2) ? ('1') : ('0'));
                 byte_ /= 2;
                 step--;
@@ -187,8 +183,8 @@ int main(/*int argc, char *argv[]*/) {
      * New generations
      */
     int gen = 0;
-    for(int i = 0; i < gen; i++) {
-        next(height, width, Colors);
+    for (int i = 0; i < gen; i++) {
+        next(height, width, realWidth, Colors);
     }
     image(Colors, Palette, height, width, "kek.bmp", header, (OffsetBits - 54) / 4);
     fclose(my_picture);
