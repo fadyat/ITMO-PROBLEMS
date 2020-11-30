18ca02e82f2f3925a5d75b31b05cafb0ec48c7fa
Artyom Fadeyev <70910148+fadyat@users.noreply.github.com>
2020-11-30 14:37:40 +0300

    Update Set.cpp

--- Algorithms/I semester/Labs/LW6-HashTables/Set.cpp
+++ Algorithms/I semester/Labs/LW6-HashTables/Set.cpp
@@ -2,19 +2,21 @@
 #include <vector>
 #include <algorithm>
 #include <list>
-
+ 
 using namespace std;
-
+ 
+vector<list<int>> all;
 static const int size_ = 1e4 + 121;
-
+ 
 int hash_(int &x) {
     return abs(x % size_);
 }
-
-string exist_(int &x, int &where, vector<list<int>> &all) {
+ 
+string exist_(int &x) {
+    int i = hash_(x);
     bool have = false;
-    if ((int) all[where].size() != 0) {
-        for (auto it = all[where].begin(); it != all[where].end(); it++) {
+    if (!all[i].empty()) {
+        for (auto it = all[i].begin(); it != all[i].end(); it++) {
             if (*it == x) {
                 have = true;
                 break;
@@ -23,18 +25,19 @@ string exist_(int &x, int &where, vector<list<int>> &all) {
     }
     return ((have) ? ("true") : ("false"));
 }
-
-void delete_(int &x, int &where, vector<list<int>> &all) {
-    if ((int) all[where].size() != 0) {
-        for (auto it = all[where].begin(); it != all[where].end(); it++) {
+ 
+void delete_(int &x) {
+    int i = hash_(x);
+    if (!all[i].empty()) {
+        for (auto it = all[i].begin(); it != all[i].end(); it++) {
             if (*it == x) {
-                all[where].erase(it);
+                all[i].erase(it);
                 break;
             }
         }
     }
 }
-
+ 
 int main() {
     freopen("set.in", "r", stdin);
     freopen("set.out", "w", stdout);
@@ -42,21 +45,21 @@ int main() {
     cin.tie(nullptr);
     cout.tie(nullptr);
     string s;
-    vector<list<int>> all(size_);
+    all.resize(size_);
     while (cin >> s) {
         int x;
         cin >> x;
-        int where = hash_(x);
         if (s[0] == 'i') {
-            if (exist_(x, where, all) == "false") {
-                all[where].push_back(x);
+            if (exist_(x) == "false") {
+                int i = hash_(x);
+                all[i].push_back(x);
             }
         }
         else if (s[0] == 'e') {
-            cout << exist_(x, where, all) << '\n';
+            cout << exist_(x) << '\n';
         }
         else if (s[0] == 'd') {
-            delete_(x, where, all);
+            delete_(x);
         }
     }
 }
