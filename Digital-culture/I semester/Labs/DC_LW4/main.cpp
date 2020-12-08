143b707f881cb7210d8c71ccfa985317d4def37c
Artyom Fadeyev <70910148+fadyat@users.noreply.github.com>
2020-12-08 22:39:17 +0300

    Update main.cpp

--- Digital-culture/I semester/Labs/DC_LW4/main.cpp
+++ Digital-culture/I semester/Labs/DC_LW4/main.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <unordered_map>
 #include <map>
 
 using namespace std;
-// wrong
 
 vector<char> bad = {'!', '.', ',', '?', '!',
- ';', ':', '"','(', ')', '«', '»'};
+ ';', ':', '"', '(', ')', '«', '»'};
 
 bool is_it_good(char x) {
     for(int i = 0; i < (int)bad.size(); i++) {
@@ -26,7 +26,7 @@ string check(string &s) {
     for(int i = 0; i < (int)s.size(); i++) {
         if(is_it_good(s[i])) {
             if(s[i] >= 'À' && s[i] <= 'ß') {
-                s[i] = s[i] - 'A' + 'a';
+                s[i] = s[i] - 'À' + 'à';
             }
             else if(s[i] >= 'A' && s[i] <= 'Z') {
                 s[i] = s[i] - 'A' + 'a';
@@ -37,26 +37,29 @@ string check(string &s) {
     return new_s;
 }
 
-int levenshtein(string s1, string s2, int ins, int del, int rep) {
-    vector<vector<int>> D((int)s1.size(), vector<int>((int)s2.size()));
-    D[0][0] = 0;
-    for(int i = 1; i < (int)s2.size(); i++){
-            D[0][i] = D[0][i - 1] + ins;
+int levenshtein(string s1, string s2) {
+    int n = s1.size(), m = s2.size();
+    vector<int> D(s1.size() + 1);
+
+    for (int i = 0; i <= n; ++i) {
+        D[i] = i;
     }
-    for(int i = 1; i < (int)s1.size(); i++) {
-        D[i][0] = D[i - 1][0] + del;
-        for(int j = 1; j < (int)s2.size(); j++) {
-            if(s1[i] != s2[j]) {
-                D[i][j] = min(D[i - 1][j] + del,
-                              min(D[i][j - 1] + ins,
-                                  D[i - 1][j - 1] + rep));
+
+    for (int j = 1; j <= m; ++j) {
+        int prev = D[0], tmp;
+        ++D[0];
+        for (int i = 1; i <= n; ++i) {
+            tmp = D[i];
+            if (s1[i - 1] == s2[j - 1]) {
+                D[i] = prev;
             }
             else {
-                D[i][j] = D[i - 1][j - 1];
+                D[i] = min(min(D[i - 1], D[i]), prev) + 1;
             }
+            prev = tmp;
         }
     }
-    return D[(int)s1.size() - 1][(int)s2.size() - 1];
+    return D[n];
 }
 
 int main() {
@@ -64,7 +67,7 @@ int main() {
     ifstream in("input.txt");
     in.is_open();
     string s;
-    map<string, pair<int, string>> all;
+    unordered_map<string, pair<int, string>> all;
     vector<string> my_ans;
     while(in >> s) {
         string new_s = check(s);
@@ -126,13 +129,20 @@ int main() {
     int yet = 0;
     for(int i = 0; i < not_here.size(); i++) {
         int ans = 1000;
+        int check_value = 0;
         for(auto j = dict.begin(); j != dict.end(); j++) {
             // here I count Levenshtein distance for my wrong wordform
-            int tmp = levenshtein(not_here[i].first, (*j).first, 1, 1, 1);
+            int tmp = levenshtein(not_here[i].first, (*j).first);
             if(tmp < ans) {
                 not_here[i].second = (*j).first;
                 ans = tmp;
             }
+            else if (tmp == ans && (*j).second > check_value) {
+                not_here[i].second = (*j).first;
+                ans = tmp;
+                check_value = (*j).second;
+            }
+            //
         }
         cout << not_here[i].first << " - ";
         if(ans <= 2) {
