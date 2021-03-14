a45719700204f83c8590b97d7c6efc482056e4ec
Artyom Fadeyev <70910148+fadyat@users.noreply.github.com>
2021-03-14 16:42:38 +0300

    Update process.cpp

--- Programming/C++/Labs/lab1/process.cpp
+++ Programming/C++/Labs/lab1/process.cpp
@@ -26,8 +26,11 @@ public:
     void show () const {
         cout << x << " " << y << endl;
     }
-    static double distance (Point &p1, Point &p2) {
-        return sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y));
+    [[nodiscard]] int getX () const {
+        return x;
+    }
+    [[nodiscard]] int getY () const {
+        return y;
     }
 };
 
@@ -57,55 +60,138 @@ public:
 };
 
 class LockedLine : public Line {
-private:
-    double P = 0;
-    static double countP(vector<Point> &points_) {
+protected:
+    double P = -1;
+    double countP() {
+        int n = points.size();
         double s = 0;
-        for (int i = 0; i < points_.size(); i++) {
-            s += points_[i].distance(points_[i], points_[(i + 1) % points_.size()]);
+        for (int i = 0; i < n; i++) {
+            int j = (i + 1) % n;
+            double x1 = points[i].getX(), y1 = points[i].getY();
+            double x2 = points[j].getX(), y2 = points[j].getY();
+            s += sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
         }
         return s;
     }
 public:
-    explicit LockedLine (vector<Point> &points_) : Line(points_), P(countP(points_)) {}
+    explicit LockedLine (vector<Point> &points_) : Line(points_), P(countP()) {}
     LockedLine () = default;
     LockedLine (const LockedLine &other) : Line(other) {
         this->P = other.P;
     }
     LockedLine& operator = (const LockedLine &other) {
+        if (this == &other) {
+            return *this;
+        }
         Line::operator = (other);
         this->P = other.P;
         return *this;
     }
-    void itsP () const {
-        cout << "P: \n" << P << endl;
+    [[nodiscard]] double itsP () const {
+        return P;
     }
 };
 
-int main() {/*
+class Polygon : public LockedLine {
+protected:
+    double S = -1;
+    double countS () {
+        double s = 0;
+        int n = points.size();
+        for (int i = 0; i < n; i++) {
+            int j = (i + 1) % n;
+            double x1 = points[i].getX(), y1 = points[i].getY();
+            double x2 = points[j].getX(), y2 = points[j].getY();
+            s += x1 * y2;
+            s -= x2 * y1;
+        }
+        return s / 2;
+    }
+    bool itsConvex () {
+        int n = points.size();
+        if (n < 3) {
+            return false;
+        }
+        bool sign1, sign;
+        for (int i = 0; i < n; i++) {
+            int j = (n + i - 1) % n, k = (n + i + 1) % n;
+            int x1 = points[j].getX(), y1 = points[j].getY();
+            int x2 = points[i].getX(), y2 = points[i].getY();
+            int x3 = points[k].getX(), y3 = points[k].getY();
+            Point ab (x2 - x1, y2 - y1);
+            Point bc (x3 - x2, y3 - y2);
+            double cnt = ab.getX() * bc.getY() - ab.getY() * bc.getX();
+            sign = (cnt >= 0);
+            if (i == 0) {
+                sign1 = (cnt >= 0);
+            }
+            if (sign != sign1) {
+                return false;
+            }
+        }
+        return true;
+    }
+public:
+    explicit Polygon (vector<Point> &points_) : LockedLine(points_) {
+        if (!itsConvex()) {
+            cout << "Incorrect data" << endl;
+        }
+        else {
+            cout << "Data is correct" << endl;
+            S = countS();
+        }
+    }
+    Polygon () = default;
+    Polygon (const Polygon &other) : LockedLine(other) {
+        this->S = other.S;
+    }
+    Polygon& operator = (const Polygon &other) {
+        if (this == &other) {
+            return *this;
+        }
+        LockedLine::operator=(other);
+        this->S = other.S;
+        return *this;
+    }
+    [[nodiscard]] double itsS () const {
+        return S;
+    }
+};
+
+int main() {
+    /*
     vector<Point> all;
     all.emplace_back(0, 0);
     all.emplace_back(2, 0);
     all.emplace_back(2, 2);
     all.emplace_back(0, 2);
     LockedLine line2(all);
-    line2.itsP();
-    line2.show();
+    cout << line2.itsP() << endl;
+    line2.show(); */
     vector<Point> all1;
     all1.emplace_back(0, 0);
     all1.emplace_back(3, 0);
     all1.emplace_back(3, 3);
-    all1.emplace_back(0, 3);
+    all1.emplace_back(2, 1);
+    /*
     LockedLine line1 (all1);
-    line1.itsP();
+    cout << line1.itsP() << endl;
     line1.show();
     LockedLine line3 = line1;
-    line3.itsP();
+    cout << line3.itsP() << endl;
     line3.show();
     line3 = line1;
-    line3.itsP();
+    cout << line3.itsP() << endl;
     line3.show();
     line3 = line2;
-    line3.itsP();
+    cout << line3.itsP() << endl;
     line3.show();*/
+    Polygon x (all1);
+    cout << x.itsS() << endl;
+    cout << x.itsP() << endl;
+    x.show();
+    /*    Polygon yy;
+    cout << yy.itsS() << endl;
+    cout << yy.itsP() << endl;
+    yy.show();*/
 }
