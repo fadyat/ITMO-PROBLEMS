c605c108ee97d0efbb7c5a2acdb8342254e5fec3
fadyat <fadyat@icloud.com>
2021-05-14 18:38:42 +0300

    jump to gl/gl.h

--- Programming/C++/Labs/lab6/src/main.cpp
+++ Programming/C++/Labs/lab6/src/main.cpp
@@ -1,53 +1,169 @@
-#include <iostream>
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
+#include <windows.h>
+#include <gl/gl.h>
 
-using namespace std;
+LRESULT CALLBACK WindowProc(HWND, UINT, WPARAM, LPARAM);
 
-static const int WIDTH = 800;
-static const int HEIGHT = 600;
+void EnableOpenGL(HWND hwnd, HDC *, HGLRC *);
 
-int main()
-{
-    // Init GLFW
-    if (!glfwInit()) {
-        cout << "glfwInit()\n";
-        return -1;
+void DisableOpenGL(HWND, HDC, HGLRC);
+
+
+int WINAPI WinMain(HINSTANCE hInstance,
+                   HINSTANCE hPrevInstance,
+                   LPSTR lpCmdLine,
+                   int nCmdShow) {
+    WNDCLASSEX wcex;
+    HWND hwnd;
+    HDC hDC;
+    HGLRC hRC;
+    MSG msg;
+    BOOL bQuit = FALSE;
+    float theta = 0.0f;
+
+    /* register window class */
+    wcex.cbSize = sizeof(WNDCLASSEX);
+    wcex.style = CS_OWNDC;
+    wcex.lpfnWndProc = WindowProc;
+    wcex.cbClsExtra = 0;
+    wcex.cbWndExtra = 0;
+    wcex.hInstance = hInstance;
+    wcex.hIcon = LoadIcon(nullptr, IDI_APPLICATION);
+    wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground = (HBRUSH) GetStockObject(BLACK_BRUSH);
+    wcex.lpszMenuName = nullptr;
+    wcex.lpszClassName = "GLSample";
+    wcex.hIconSm = LoadIcon(nullptr, IDI_APPLICATION);
+
+
+    if (!RegisterClassEx(&wcex))
+        return 0;
+
+    /* create main window */
+    hwnd = CreateWindowEx(0,
+                          "GLSample",
+                          "OpenGL Sample",
+                          WS_OVERLAPPEDWINDOW,
+                          CW_USEDEFAULT,
+                          CW_USEDEFAULT,
+                          256,
+                          256,
+                          nullptr,
+                          nullptr,
+                          hInstance,
+                          nullptr);
+
+    ShowWindow(hwnd, nCmdShow);
+
+    /* enable OpenGL for the window */
+    EnableOpenGL(hwnd, &hDC, &hRC);
+
+    /* program main loop */
+    while (!bQuit) {
+        /* check for messages */
+        if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE)) {
+            /* handle or dispatch messages */
+            if (msg.message == WM_QUIT) {
+                bQuit = TRUE;
+            } else {
+                TranslateMessage(&msg);
+                DispatchMessage(&msg);
+            }
+        } else {
+            /* OpenGL animation code goes here */
+
+            glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+            glClear(GL_COLOR_BUFFER_BIT);
+
+            glPushMatrix();
+            glRotatef(theta, 0.0f, 0.0f, 1.0f);
+
+            glBegin(GL_TRIANGLES);
+
+            glColor3f(1.0f, 0.0f, 0.0f);
+            glVertex2f(0.0f, 1.0f);
+            glColor3f(0.0f, 1.0f, 0.0f);
+            glVertex2f(0.87f, -0.5f);
+            glColor3f(0.0f, 0.0f, 1.0f);
+            glVertex2f(-0.87f, -0.5f);
+
+            glEnd();
+
+            glPopMatrix();
+
+            SwapBuffers(hDC);
+
+            theta += 1.0f;
+            Sleep(1);
+        }
     }
-    /* Options */
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
-    /* Window */
-    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "LearnOpenGL", nullptr, nullptr);
-    if (!window) {
-        cout << "window\n";
-        glfwTerminate();
-        return -1;
+    /* shutdown OpenGL */
+    DisableOpenGL(hwnd, hDC, hRC);
+
+    /* destroy the window explicitly */
+    DestroyWindow(hwnd);
+
+    return (int)msg.wParam;
+}
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+    switch (uMsg) {
+        case WM_CLOSE:
+            PostQuitMessage(0);
+            break;
+
+        case WM_DESTROY:
+            return 0;
+
+        case WM_KEYDOWN: {
+            switch (wParam) {
+                case VK_ESCAPE:
+                    PostQuitMessage(0);
+                    break;
+            }
+        }
+            break;
+
+        default:
+            return DefWindowProc(hwnd, uMsg, wParam, lParam);
     }
-    glfwMakeContextCurrent(window);
 
-    /* GL */
-    if (!gladLoadGL()) {
-        cout << "gladLoadGL()\n";
-        return -1;
-    }
-
-
-    while (!glfwWindowShouldClose(window))
-    {
-        glfwPollEvents();
-
-        glClearColor(0.3f, 0.3f, 0.3f, 1.0f);
-
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        glfwSwapBuffers(window);
-    }
-
-
-    glfwTerminate();
     return 0;
 }
+
+void EnableOpenGL(HWND hwnd, HDC *hDC, HGLRC *hRC) {
+    PIXELFORMATDESCRIPTOR pfd;
+
+    int iFormat;
+
+    /* get the device context (DC) */
+    *hDC = GetDC(hwnd);
+
+    /* set the pixel format for the DC */
+    ZeroMemory(&pfd, sizeof(pfd));
+
+    pfd.nSize = sizeof(pfd);
+    pfd.nVersion = 1;
+    pfd.dwFlags = PFD_DRAW_TO_WINDOW |
+                  PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER;
+    pfd.iPixelType = PFD_TYPE_RGBA;
+    pfd.cColorBits = 24;
+    pfd.cDepthBits = 16;
+    pfd.iLayerType = PFD_MAIN_PLANE;
+
+    iFormat = ChoosePixelFormat(*hDC, &pfd);
+
+    SetPixelFormat(*hDC, iFormat, &pfd);
+
+    /* create and enable the render context (RC) */
+    *hRC = wglCreateContext(*hDC);
+
+    wglMakeCurrent(*hDC, *hRC);
+}
+
+void DisableOpenGL(HWND hwnd, HDC hDC, HGLRC hRC) {
+    wglMakeCurrent(nullptr, nullptr);
+    wglDeleteContext(hRC);
+    ReleaseDC(hwnd, hDC);
+}
+
