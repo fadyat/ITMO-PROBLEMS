d6ecbe477db677cf8e9d3fb41ba1c50487ba22cb
fadyat <fadyat@icloud.com>
2021-05-17 05:20:25 +0300

    finally create rotations of cube

--- Programming/C++/Labs/lab6/external/glad/include/glad/glad.h
+++ Programming/C++/Labs/lab6/external/glad/include/glad/glad.h
@@ -24,7 +24,6 @@
 #define __glad_h_
 
 #ifdef __gl_h_
-#error OpenGL header already included, remove this include, glad already provides it
 #endif
 #define __gl_h_
 
--- Programming/C++/Labs/lab6/src/main.cpp
+++ Programming/C++/Labs/lab6/src/main.cpp
@@ -8,28 +8,26 @@ using namespace std;
 static int WIDTH = 1024;
 static int HEIGHT = 1024;
 
-float vertex[] = {1, 1, 0,  1, -1, 0,  -1, -1, 0,  -1, 1, 0};
-
-float degree_x = 68, degree_z = 44.5, location_z = -8.39999;
+float angle_x = 68, angle_z = 44.5, location_z = -8.39999;
 POINTFLOAT position = {13.7699, -10.6143};
 
 void movement() {
     /* move camera */
     if(GetKeyState('I') < 0) {
-        degree_x += 0.5;
+        angle_x += 0.5;
     }
     else if(GetKeyState('K') < 0) {
-        degree_x -= 0.5;
+        angle_x -= 0.5;
     }
     else if(GetKeyState('J') < 0) {
-        degree_z += 0.5;
+        angle_z += 0.5;
     }
     else if(GetKeyState('L') < 0) {
-        degree_z -= 0.5;
+        angle_z -= 0.5;
     }
 
     /* move me */
-    double alpha = -degree_z / 180 * M_PI;
+    double alpha = -angle_z / 180 * M_PI;
     float speed = 0;
     if (GetKeyState('W') < 0) {
         speed = 0.1;
@@ -57,10 +55,17 @@ void movement() {
         position.y += float(cos(alpha) * speed);
     }
 
-    glRotatef(-degree_x, 1, 0, 0);
-    glRotatef(-degree_z, 0, 0, 1);
+    glRotatef(-angle_x, 1, 0, 0);
+    glRotatef(-angle_z, 0, 0, 1);
     glTranslatef(-position.x, -position.y, location_z);
 }
+
+/*
+ *  The planes of the cube go in the order:
+ *      top     down    front   back    left    right
+ */
+
+int color_hex[9] = {0xFFFFFF, 0xFFFF00, 0xFF0F00, 0xFF931C, 0x25FF00, 0x3155EC};
 
 class little_cube {
 private:
@@ -73,23 +78,7 @@ public:
         memset(color, 0, sizeof(color));
     }
 
-    // rotation of the cube relative to the XOZ plane by 90 degrees clockwise
-    void rotate_x() {
-        // 012345 -> 452310
-        swap(color[0], color[4]); // 412305
-        swap(color[1], color[5]); // 452301
-        swap(color[4], color[5]); // 452310
-    }
-
-    // rotation of the cube relative to the YOZ plane by 90 degrees clockwise
-    void rotate_y() {
-        // 012345 -> 231045
-        swap(color[0], color[2]); // 210345
-        swap(color[1], color[3]); // 230145
-        swap(color[2], color[3]); // 231045
-    }
-
-    // rotation of the cube relative to the XOY plane by 90 degrees clockwise
+    /* rotation of the cube relative to the *** plane by 90 angles clockwise */
     void rotate_z() {
         // 012345 -> 015423
         swap(color[2], color[5]); // 015342
@@ -97,12 +86,25 @@ public:
         swap(color[4], color[5]); // 015423
     };
 
+    void rotate_y() {
+        // 012345 -> 231045
+        swap(color[0], color[2]); // 210345
+        swap(color[1], color[3]); // 230145
+        swap(color[2], color[3]); // 231045
+    }
+
+    void rotate_x() {
+        // 012345 -> 452310
+        swap(color[0], color[4]); // 412305
+        swap(color[1], color[5]); // 452301
+        swap(color[4], color[5]); // 452310
+    }
+
     void set_color(int plane_number, int color_) {
         this->color[plane_number] = color_;
     }
 
     unsigned char *my_color_hex(int plane_number) {
-        // divide color[plane_number] on rgb
         rgb[0] = color[plane_number] >> 16;
         rgb[1] = color[plane_number] >> 8;
         rgb[2] = color[plane_number];
@@ -114,42 +116,36 @@ public:
         glTranslatef(x, y, z);
         glBegin(GL_QUADS);
 
-        // up
         glColor3ubv(my_color_hex(0));
         glVertex3f(edge_size, edge_size, edge_size);
         glVertex3f(0, edge_size, edge_size);
         glVertex3f(0, 0, edge_size);
         glVertex3f(edge_size, 0, edge_size);
 
-        // down
         glColor3ubv(my_color_hex(1));
         glVertex3f(edge_size, 0, 0);
         glVertex3f(0, 0, 0);
         glVertex3f(0, edge_size, 0);
         glVertex3f(edge_size, edge_size, 0);
 
-        // front
         glColor3ubv(my_color_hex(2));
         glVertex3f(edge_size, 0, edge_size);
         glVertex3f(0, 0, edge_size);
         glVertex3f(0, 0, 0);
         glVertex3f(edge_size, 0, 0);
 
-        // back
         glColor3ubv(my_color_hex(3));
         glVertex3f(edge_size, edge_size, 0);
         glVertex3f(0, edge_size, 0);
         glVertex3f(0, edge_size, edge_size);
         glVertex3f(edge_size, edge_size, edge_size);
 
-        // left
         glColor3ubv(my_color_hex(4));
         glVertex3f(0, edge_size, edge_size);
         glVertex3f(0, edge_size, 0);
         glVertex3f(0, 0, 0);
         glVertex3f(0, 0, edge_size);
 
-        // right
         glColor3ubv(my_color_hex(5));
         glVertex3f(edge_size, edge_size, 0);
         glVertex3f(edge_size, edge_size, edge_size);
@@ -159,18 +155,21 @@ public:
         glEnd();
         glPopMatrix();
     }
-
-    ~little_cube() = default;
 };
-
-int color_hex[9] = {0xFFFFFF, 0xFFFF00, 0xFF0F00, 0xFF931C, 0x25FF00, 0x3155EC};
 
 class big_cube {
 private:
     little_cube me[3][3][3];
+    little_cube tmp_face[3][3];
+    bool fixed[3][3][3];
+    float angle_of_face[6];
     float edge_size;
+    float angle;
+    int cube_face;
+
 public:
-    explicit big_cube(float edge_size_) : edge_size(edge_size_), me() {
+    explicit big_cube(float edge_size_) : edge_size(edge_size_), me(), fixed(), cube_face(-1), angle_of_face(), angle(3) {
+        memset(angle_of_face, 0, sizeof(angle_of_face));
         // set little_cubes egde_sizes
         for (int x = 0; x < 3; x++) {
             for (int y = 0; y < 3; y++) {
@@ -180,64 +179,193 @@ public:
             }
         }
 
-        // up
         for (int x = 0; x < 3; x++) {
             for (int y = 0; y < 3; y++) {
                 me[x][y][2].set_color(0, color_hex[0]);
-            }
-        }
-
-        // down
-        for (int x = 0; x < 3; x++) {
-            for (int y = 0; y < 3; y++) {
                 me[x][y][0].set_color(1, color_hex[1]);
             }
         }
 
-        // front
         for (int x = 0; x < 3; x++) {
             for (int z = 0; z < 3; z++) {
                 me[x][0][z].set_color(2, color_hex[2]);
-            }
-        }
-
-        //back
-        for (int x = 0; x < 3; x++) {
-            for (int z = 0; z < 3; z++) {
                 me[x][2][z].set_color(3, color_hex[3]);
             }
         }
 
-        //left
         for (int y = 0; y < 3; y++) {
             for (int z = 0; z < 3; z++) {
                 me[0][y][z].set_color(4, color_hex[4]);
-            }
-        }
-
-        //right
-        for (int y = 0; y < 3; y++) {
-            for (int z = 0; z < 3; z++) {
                 me[2][y][z].set_color(5, color_hex[5]);
             }
         }
     }
 
     void show() {
+        memset(fixed, true, sizeof(fixed));
+
+        glPushMatrix();
+        int u = (cube_face & 1) * 2;
+        // XOY
+        if (cube_face == 0 || cube_face == 1) {
+            for (int x = 0; x < 3; x++) {
+                for (int y = 0; y < 3; y++) {
+                    fixed[x][y][u] = false;
+                }
+            }
+            glTranslatef(edge_size / 2, edge_size / 2, 0);
+            glRotatef(float(angle_of_face[cube_face]), 0, 0, 1);
+            glTranslatef(-edge_size / 2, -edge_size / 2, 0);
+            for (int x = 0; x < 3; x++) {
+                for (int y = 0; y < 3; y++) {
+                    me[x][y][u].draw_translated(edge_size * float(x) / 3,
+                                                edge_size * float(y) / 3,
+                                                edge_size * float(u) / 3);
+                }
+            }
+        }
+        // XOZ
+        else if (cube_face == 2 || cube_face == 3) {
+            for (int x = 0; x < 3; x++) {
+                for (int z = 0; z < 3; z++) {
+                    fixed[x][u][z] = false;
+                }
+            }
+            glTranslatef(edge_size / 2, 0, edge_size / 2);
+            glRotatef(float(angle_of_face[cube_face]), 0, 1, 0);
+            glTranslatef(-edge_size / 2, 0, -edge_size / 2);
+            for (int x = 0; x < 3; x++) {
+                for (int z = 0; z < 3; z++) {
+                    me[x][u][z].draw_translated(edge_size * float(x) / 3,
+                                                edge_size * float(u) / 3,
+                                                edge_size * float(z) / 3);
+                }
+            }
+        }
+        // YOZ
+        else if (cube_face == 4 || cube_face == 5) {
+            for (int y = 0; y < 3; y++) {
+                for (int z = 0; z < 3; z++) {
+                    fixed[u][y][z] = false;
+                }
+            }
+            glTranslatef(0, edge_size / 2, edge_size / 2);
+            glRotatef(float(angle_of_face[cube_face]), 1, 0, 0);
+            glTranslatef(0, -edge_size / 2, -edge_size / 2);
+            for (int y = 0; y < 3; y++) {
+                for (int z = 0; z < 3; z++) {
+                    me[u][y][z].draw_translated(edge_size * float(u) / 3,
+                                                edge_size * float(y) / 3,
+                                                edge_size * float(z) / 3);
+                }
+            }
+        }
+        glPopMatrix();
+
         for (int x = 0; x < 3; x++) {
             for (int y = 0; y < 3; y++) {
                 for (int z = 0; z < 3; z++) {
-                    me[x][y][z].draw_translated(edge_size / 3 * float(x),
-                                                edge_size / 3 * float(y),
-                                                edge_size / 3 * float(z));
+                    if(fixed[x][y][z]) {
+                        me[x][y][z].draw_translated(edge_size / 3 * float(x),
+                                                    edge_size / 3 * float(y),
+                                                    edge_size / 3 * float(z));
+                    }
                 }
             }
         }
     }
+
+    void cube_rotations() {
+        int key = 0;
+        if(GetKeyState('1') < 0) {
+            key = 1;
+        }
+        else if(GetKeyState('2') < 0) {
+            key = 2;
+        }
+        else if(GetKeyState('3') < 0) {
+            key = 3;
+        }
+        else if(GetKeyState('4') < 0) {
+            key = 4;
+        }
+        else if(GetKeyState('5') < 0) {
+            key = 5;
+        }
+        else if(GetKeyState('6') < 0) {
+            key = 6;
+        }
+        rotate_face(--key);
+    }
+
+    void rotate_face(int face_number) {
+        if (cube_face == -1 || cube_face == face_number) {
+            angle_of_face[face_number] += angle;
+            angle_of_face[face_number] = float(int(angle_of_face[face_number]) % 90);
+            if (int(angle_of_face[face_number])) {
+                cube_face = face_number;
+            }
+            else {
+                int swaps = ((cube_face == 2 || cube_face == 3) ? (1) : (3));
+                is_full_rotate(face_number, swaps);
+            }
+        }
+    }
+
+    void is_full_rotate(int face_number, int color_changes) {
+        while(color_changes--) {
+            int u = (face_number & 1) * 2;
+            if (face_number == 0 || face_number == 1) {
+                for (int x = 0; x < 3; x++) {
+                    for (int y = 0; y < 3; y++) {
+                        tmp_face[y][2 - x] = me[x][y][u];
+                    }
+                }
+                for (int x = 0; x < 3; x++) {
+                    for (int y = 0; y < 3; y++) {
+                        tmp_face[x][y].rotate_z();
+                        me[x][y][u] = tmp_face[x][y];
+                    }
+                }
+            }
+            else if (face_number == 2 || face_number == 3) {
+                for (int x = 0; x < 3; x++) {
+                    for (int z = 0; z < 3; z++) {
+                        tmp_face[z][2 - x] = me[x][u][z];
+                    }
+                }
+                for (int x = 0; x < 3; x++) {
+                    for (int z = 0; z < 3; z++) {
+                        tmp_face[x][z].rotate_x();
+                        me[x][u][z] = tmp_face[x][z];
+                    }
+                }
+            }
+            else if (face_number == 4 || face_number == 5) {
+                for (int y = 0; y < 3; y++) {
+                    for (int z = 0; z < 3; z++) {
+                        tmp_face[z][2 - y] = me[u][y][z];
+                    }
+                }
+                for (int y = 0; y < 3; y++) {
+                    for (int z = 0; z < 3; z++) {
+                        tmp_face[y][z].rotate_y();
+                        me[u][y][z] = tmp_face[y][z];
+                    }
+                }
+            }
+        }
+        angle_of_face[face_number] = 0;
+        cube_face = -1;
+    }
+
+    void while_not_90() {
+        if(cube_face != -1) {
+            rotate_face(cube_face);
+        }
+        show();
+    }
 };
-
-
-
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
     switch (uMsg) {
@@ -260,37 +388,24 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) 
         default:
             return DefWindowProc(hwnd, uMsg, wParam, lParam);
     }
-
     return 0;
 }
 
 void EnableOpenGL(HWND hwnd, HDC *hDC, HGLRC *hRC) {
     PIXELFORMATDESCRIPTOR pfd;
-
     int iFormat;
-
-    /* get the device context (DC) */
     *hDC = GetDC(hwnd);
-
-    /* set the pixel format for the DC */
     ZeroMemory(&pfd, sizeof(pfd));
-
     pfd.nSize = sizeof(pfd);
     pfd.nVersion = 1;
-    pfd.dwFlags = PFD_DRAW_TO_WINDOW |
-                  PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER;
+    pfd.dwFlags = PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER;
     pfd.iPixelType = PFD_TYPE_RGBA;
     pfd.cColorBits = 24;
     pfd.cDepthBits = 16;
     pfd.iLayerType = PFD_MAIN_PLANE;
-
     iFormat = ChoosePixelFormat(*hDC, &pfd);
-
     SetPixelFormat(*hDC, iFormat, &pfd);
-
-    /* create and enable the render context (RC) */
     *hRC = wglCreateContext(*hDC);
-
     wglMakeCurrent(*hDC, *hRC);
 }
 
@@ -327,7 +442,6 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
         return 0;
     }
 
-    /* create main window */
     hwnd = CreateWindowEx(0, "GLSample", "CUB",
                           WS_OVERLAPPEDWINDOW, CW_USEDEFAULT,
                           CW_USEDEFAULT, WIDTH, HEIGHT, nullptr,
@@ -335,42 +449,36 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 
     ShowWindow(hwnd, nCmdShow);
 
-    /* enable OpenGL for the window */
+
     EnableOpenGL(hwnd, &hDC, &hRC);
     glEnable(GL_DEPTH_TEST);
     glFrustum(-1, 1,  -1, 1,  3, 100);
     big_cube cube(3);
 
-    /* program main loop */
     while (!bQuit) {
-        /* check for messages */
         if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE)) {
-            /* handle or dispatch messages */
             if (msg.message == WM_QUIT) {
                 bQuit = TRUE;
-            } else {
+            }
+            else {
                 TranslateMessage(&msg);
                 DispatchMessage(&msg);
             }
-        } else {
-            /* OpenGL animation code goes here */
-
+        }
+        else {
             glClearColor(0.76953125f, 0.8125f, 0.8984375f, 0);
             glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
             glPushMatrix();
             movement();
-            cube.show();
+            cube.while_not_90();
+            cube.cube_rotations();
             glPopMatrix();
 
             SwapBuffers(hDC);
         }
     }
-    /* shutdown OpenGL */
     DisableOpenGL(hwnd, hDC, hRC);
-
-    /* destroy the window explicitly */
     DestroyWindow(hwnd);
-
     return (int) msg.wParam;
 }
