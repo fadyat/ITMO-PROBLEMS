f7f3de25feeb4e6cf3b7a9f49a88cefa62f22391
fadyat <fadyat@icloud.com>
2021-09-16 20:25:23 +0300

    write lab1, don't check on mistakes

--- OOP/III semester/Solutions/_1Shops/Program.cs
+++ OOP/III semester/Solutions/_1Shops/Program.cs
@@ -1,9 +1,29 @@
-﻿namespace Shops
+﻿using System;
+using Shops.Classes;
+
+namespace Shops
 {
     internal class Program
     {
         private static void Main()
         {
+            /*
+
+             ShopManager ---- List<Shop> _shops + List<Product> _registeredProducts
+             |
+             |_ (CreateShop, RegisterProduct, CheapProductSearch, DeliverProducts, SetProductsPrices)
+                 |            |
+                 |            |
+                 |            |_ Product ---- _price + _name
+                 |
+                 |_ Shop ---- _id + _name + _address + Dictionary<Product, ProductQuantity> _storedProducts
+
+
+             Customer ---- _name + _money + Dictionary<Product, ProductQuantity> _purchasedProducts
+             |
+             |_ (PurchaseProduct)
+
+             */
         }
     }
 }
