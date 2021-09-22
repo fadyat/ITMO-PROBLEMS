fe7bd44289c995682fac2cc02d0ddc8fa25dc01c
Artyom Fadeyev <70910148+fadyat@users.noreply.github.com>
2021-09-22 14:19:09 +0300

    test.sh
    # rename all files in directory with first line equal to
    # "#!/bin/bash" -> change his extension on *.sh

---
+++ OS/III semester/Solutions/lab0/test.sh
@@ -0,0 +1,18 @@
+# rename all files in directory with first line equal to
+# "#!/bin/bash" -> change his extension on *.sh
+
+#!/bin/bash
+correctLine="#!/bin/bash"
+for file in $PWD/*
+do
+ line=$(head -n 1 $file)
+ if [[ $line == $correctLine ]]
+  then
+   tmp=`echo "$file" | sed 's/\.[[:lower:]]\{1,\}//g'`
+   if [[ "$file" != "$tmp.sh" ]]
+    then 
+     mv "$file" "$tmp.sh"
+   fi
+ fi
+done
+exit 0
