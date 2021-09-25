4a835facfae1c54c7a5d49446bd770ba2fbb6fc2
fadyat <fadyat@icloud.com>
2021-09-25 20:55:28 +0300

    src4.sh

---
+++ OS/III semester/Solutions/lab2/src4.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+touch src4_data
+touch tmp
+first=true
+for pid in $(ps -A -o pid)
+do
+ statusFile="/proc/$pid/status"
+ schedFile="/proc/$pid/sched"
+ ppid=$(grep -s 'PPid' $statusFile | awk '{print $2}')
+ runtime=$(grep -s 'sum_exec_runtime' $schedFile | awk '{print $3}')
+ switches=$(grep -s 'nr_switches' $schedFile | awk '{print $3}')
+ if [[ $ppid != "" ]] && [[ $runtime != "" ]] && [[ $switches != "" ]]
+  then
+   art=`bc -l <<< "($runtime) / ($switches)" | 
+   awk '{
+    printf("%f\n", $0)
+   }'`
+   if [[ $first == true ]]
+    then
+     echo "$pid $ppid $art" > tmp
+     first=false
+    else
+     echo "$pid $ppid $art" >> tmp
+   fi
+ fi
+done
+sort -n -k2 tmp |
+awk '{
+ print "ProcessID=" $1 " : Parent_ProcessID=" $2 " : Average_Running_Time=" $3
+}' > src4_data
+rm tmp
+exit 0
