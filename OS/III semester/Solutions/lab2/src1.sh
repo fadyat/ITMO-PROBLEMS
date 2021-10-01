143bcaefd3b63ac104705cd906dd3538d141eb03
Artyom Fadeyev <70910148+fadyat@users.noreply.github.com>
2021-10-01 22:10:20 +0300

    ?

--- OS/III semester/Solutions/lab2/src1.sh
+++ OS/III semester/Solutions/lab2/src1.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
-ps -U "root" -o pid,comm |
-sed 's/^ \{1,\}//g' |
-sed 's/ /:/g' > tmp
-wc -l tmp |
-awk '{
- print "Processes: " $1 "\n"
-}' > src1_data
+ps -U "root" -o pid,comm | awk '{ print $1 ":" $2 }' > tmp
+wc -l tmp | awk '{ print "Processes: " $1 "\n" }' > src1_data
 cat tmp >> src1_data
 rm tmp
 exit 0
