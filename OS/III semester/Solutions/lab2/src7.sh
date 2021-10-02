9e100e64e460458ba030f1decbdff84ba6bdd295
fadyat <fadyat@icloud.com>
2021-10-02 23:35:25 +0300

    finally done

---
+++ OS/III semester/Solutions/lab2/src7.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+for pid in $(ps -A -o pid=); do
+ etime=`ps -p $pid -o etimes=`
+ seconds=60
+ if [[ $etime -le $seconds ]]; then
+  io="/proc/$pid/io"
+  wrBytes=`grep -s "^write_bytes:" $io | awk '{ print $2 }'`
+  if [[ wrBytes != "" ]]; then
+   cmd=`ps -p $pid -o cmd=`
+   echo $pid : $wrBytes : $cmd >> tmp
+  fi
+ fi
+done
+sort -nk2 tmp | tail -3 > src7_data
+rm tmp
+exit 0
