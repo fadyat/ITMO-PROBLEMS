49cf2bd2ce4eead7898e7ff62e50be8351fc3505
fadyat <fadyat@icloud.com>
2021-10-04 21:55:10 +0300

    finally

--- OOP/III semester/Solutions/_1Shops.Tests/ShopManagerTest.cs
+++ OOP/III semester/Solutions/_1Shops.Tests/ShopManagerTest.cs
@@ -1,44 +1,95 @@
-/*using System.Collections.Generic;
+using System;
+using System.Collections.Generic;
 using NUnit.Framework;
-using _1Shops.Classes;
-using _1Shops.Exceptions;
+using Shops.Classes;
+using Shops.Exceptions;
 
-namespace _1Shops.Tests
+namespace Shops.Tests
 {
     public class ShopManagerTest
     {
         private ShopManager _shopManager;
 
-        [SetUp]
-        public void SetUp() { _shopManager = new ShopManager(); }
-
+        [SetUp] public void SetUp() { _shopManager = new ShopManager(); }
         
-        
-        [Test]
-        public void RegisterEqualProducts_ThrowException()
+        [Test] public void RegisterEqualProducts_ThrowException()
         {
             Assert.Catch<ShopException>(() =>
             {
-                ShopManager.RegisterProduct("pineapple");
-                ShopManager.RegisterProduct("pineapple");
+                Shop shop1 = _shopManager.CreateShop("Lenta", "Pushkin1");
+                _shopManager.RegisterProduct(shop1, new List<string> { "pineapple", "pineapple" });
             });
         }
-
-        private static readonly object[] TestCases =
-        {
-            new List<string> {"milk", "water", "apple"}
-        };
         
-        [Test, TestCaseSource(nameof(TestCases))]
-        public void AddingProductsToTheShop_ThrowException(List<string> products)
+        [Test] public void AddingEqualsProductsToTheShop()
         {
-            Assert.Catch<ShopException>(() =>
+            Shop shop1 = _shopManager.CreateShop("Lenta", "Pushkin1");
+            _shopManager.RegisterProduct(shop1, new List<string> { "apple" });
+            var apple = new Product("apple");
+            _shopManager.AddProducts(shop1, new List<(Product, ProductInfo)>
             {
-                _shopManager.CreateShop("Lenta", "Lermontova 1");
-                foreach (string pr in products)
-                    ShopManager.RegisterProduct(pr);
-                
+                (apple, new ProductInfo(10, 5.99)),
+                (new Product("apple"), new ProductInfo(10, 4.99))
             });
+            Assert.True(shop1.StoredProducts.Count == 1 && shop1.StoredProducts[apple].Cnt == 20);
+        }
+
+        [Test] public void CheapestShopFinding()
+        {
+            Shop shop1 = _shopManager.CreateShop("Lenta", "Pushkin1");
+            _shopManager.RegisterProduct(shop1, new List<string> { "apple", "sweet" });
+            _shopManager.AddProducts(shop1, new List<(Product, ProductInfo)>
+            {
+                (new Product("apple"), new ProductInfo(10, 10)),
+                (new Product("sweet"), new ProductInfo(10, 20))
+            });
+            
+            Shop shop2 = _shopManager.CreateShop("Lenta", "Pushkin1");
+            _shopManager.RegisterProduct(shop2, new List<string> { "apple", "sweet" });
+            _shopManager.AddProducts(shop2, new List<(Product, ProductInfo)>
+            {
+                (new Product("apple"), new ProductInfo(10, 20)),
+                (new Product("sweet"), new ProductInfo(10, 10))
+            });
+
+            Shop shop3 = _shopManager.CheapProductSearch(new List<(Product, uint cheapCnt)>
+            {
+                (new Product("apple"), 4),
+                (new Product("sweet"), 6)
+            });
+            Assert.True(shop2.Id == shop3.Id);
+        }
+
+        [Test] public void PurchaseProduct()
+        {
+            Shop shop1 = _shopManager.CreateShop("Lenta", "Pushkin1");
+            _shopManager.RegisterProduct(shop1, new List<string> { "apple", "sweet" });
+            _shopManager.AddProducts(shop1, new List<(Product, ProductInfo)>
+            {
+                (new Product("apple"), new ProductInfo(10, 10)),
+                (new Product("sweet"), new ProductInfo(10, 20))
+            });
+            
+            Shop shop2 = _shopManager.CreateShop("Lenta", "Pushkin1");
+            _shopManager.RegisterProduct(shop2, new List<string> { "apple", "sweet" });
+            _shopManager.AddProducts(shop2, new List<(Product, ProductInfo)>
+            {
+                (new Product("apple"), new ProductInfo(10, 20)),
+                (new Product("sweet"), new ProductInfo(10, 10))
+            });
+            var customer1 = new Customer("customer1", 1000);
+            var customer2 = new Customer("customer2", 1000);
+            _shopManager.PurchaseProduct(customer1, shop1, new List<(Product, uint purchaseCnt)>
+            {
+                (new Product("apple"), 5),
+                (new Product("sweet"), 5)
+            });
+            _shopManager.PurchaseProduct(customer2, shop2, new List<(Product, uint purchaseCnt)>
+            {
+                (new Product("apple"), 5),
+                (new Product("sweet"), 5)
+            });
+            Assert.True(Math.Abs(customer1.Money - 850) == 0 && Math.Abs(customer2.Money - 850) == 0);
         }
     }
-}*/
+}
--- OOP/III semester/Solutions/_1Shops/Classes/Shop.cs
+++ OOP/III semester/Solutions/_1Shops/Classes/Shop.cs
@@ -4,21 +4,22 @@ namespace Shops.Classes
 {
     public class Shop
     {
-        private readonly uint _id;
         private readonly string _address;
 
         public Shop() { }
         public Shop(uint shopId, string shopName, string shopAddress)
         {
-            (_id, Name, _address) = (shopId, shopName, shopAddress);
+            (Id, Name, _address) = (shopId, shopName, shopAddress);
             StoredProducts = new Dictionary<Product, ProductInfo>();
             RegisteredProducts = new HashSet<Product>();
         }
 
         public string Name { get; }
+        public uint Id { get; }
+
         public Dictionary<Product, ProductInfo> StoredProducts { get; }
         public HashSet<Product> RegisteredProducts { get; }
 
-        public override string ToString() { return Name + " " + _address + " " + _id; }
+        public override string ToString() { return Name + " " + _address + " " + Id; }
     }
 }
