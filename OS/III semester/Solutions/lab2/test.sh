16cfe8b9ce74032331514c0458adbdc1d3af26d8
fadyat <fadyat@icloud.com>
2021-10-06 11:49:09 +0300

    added test2

---
+++ OS/III semester/Solutions/lab2/test.sh
@@ -0,0 +1,15 @@
+# Have processName (ps -o cmd), u should get his pid
+
+#!/bin/bash
+processCmd=$1
+for pid in $(ps -A -o -pid=); do
+ pidFile="/proc/$pid"
+ if [[ $pidFile == "" ]]; then continue; fi
+ currentProcessCmd=`ps -p $pid -o cmd=`
+ if [[ $currentProcessCmd == $processCmd ]]; then
+  echo "PID: " $pid
+  exit 0
+ fi
+done
+echo "No such process!"
+exit 0
