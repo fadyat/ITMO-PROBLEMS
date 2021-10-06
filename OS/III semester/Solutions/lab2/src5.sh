e221e22b4710fbfe171c0c6ac023b5cb1a13942e
Artyom Fadeyev <70910148+fadyat@users.noreply.github.com>
2021-10-06 10:05:18 +0300

    ;

--- OS/III semester/Solutions/lab2/src5.sh
+++ OS/III semester/Solutions/lab2/src5.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 prevPpid=`echo "$(head -1 src4_data)" | awk -F: '{ print $2 }' | sed 's/ Parent_ProcessID=//'`
-sum=0
-cnt=0
+sum=0; cnt=0
 while read line; do
  nowPpid=`echo $line | awk -F: '{ print $2 }' | sed 's/ Parent_ProcessID=//'`
  art=`echo $line | awk -F: '{ print $3 }' | sed 's/ Average_Running_Time=//'`
  if [[ $prevPpid != nowPpid ]]; then
   avg=`bc -l <<< "($sum) / ($cnt)" | awk '{ printf("%f", $0) }'`
   echo "Average_Running_Children_of_ParentID="$prevPpid"is "$avg
-  sum=0
-  cnt=0
+  sum=0; cnt=0
  fi
  sum=`bc -l <<< "($sum) + ($art)" | awk '{ printf("%f", $0) }'`
  prevPpid=$nowPpid
