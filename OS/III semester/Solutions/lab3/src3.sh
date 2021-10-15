00a25db6dbd5fcf7386fe0a6926d7ff738d33454
Artyom Fadeyev <70910148+fadyat@users.noreply.github.com>
2021-10-15 19:55:48 +0300

    upd crontab

--- OS/III semester/Solutions/lab3/src3.sh
+++ OS/III semester/Solutions/lab3/src3.sh
@@ -1,6 +1,6 @@
 # use crontab [-l] for all crontab process from root
-# 	      [-r] for reset all root process
+# 	          [-r] for reset all root process
+#             [-e] for all started crontab processes
 
 #!/bin/bash
-echo "*/5 * * * * ./src1.sh" | crontab
-exit 0
+echo "*/5 * * * * /home/user/lab2/src1.sh" | crontab
