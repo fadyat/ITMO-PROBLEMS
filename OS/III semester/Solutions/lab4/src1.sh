c796087449af6518e8618238737d9901c0edbdac
fadyat <fadyat@icloud.com>
2021-11-13 17:00:41 +0300

    lab4 OS / 1 ?

---
+++ OS/III semester/Solutions/lab4/src1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+id="$(cat id)"
+[ $# -ne 1] && exit
+prev=$PWD/$1
+[ ! -f $prev ] && exit
+[ ! -d ~/trash ] && mkdir ~/trash
+new=~/trash/$id
+ln $prev $new
+rm $prev
+echo "$prev $id" >> ~/trash.log
+((id++))
+echo "$id" > id
