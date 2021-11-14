6e54ca37b9b54ad2decac514e2310c5ab14b15da
Artyom Fadeyev <70910148+fadyat@users.noreply.github.com>
2021-11-14 17:48:38 +0300

    Create src4.sh

---
+++ OS/III semester/Solutions/lab4/src4.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+actualBackup=`ls /home/user | grep -E "^Backup-" | sort -n | tail -1`
+restore=/home/user/restore
+[ ! -d $restore ] && mkdir $restore
+path=/home/user/$actualBackup
+backupData=`ls $path`
+for file in $backupData; do
+ copy=`echo $file | grep -E -o "[[:alnum:]._-]{1,}.[[:digit:]]{4}-[[:digit:]]{2}-[[:digit:]]{2}"`
+ [ "$copy" == "$file" ] && continue
+ cp $path/$file $restore
+done
