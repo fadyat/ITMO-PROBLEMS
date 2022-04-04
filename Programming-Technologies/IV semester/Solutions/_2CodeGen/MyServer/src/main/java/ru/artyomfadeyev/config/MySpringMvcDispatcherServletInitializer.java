d9e778b1bf9f8b0308ad0a7847e2c598ceec2941
fadyat <fadyat@icloud.com>
2022-04-04 16:55:41 +0300

    Java server start

---
+++ Programming-Technologies/IV semester/Solutions/_2CodeGen/MyServer/src/main/java/ru/artyomfadeyev/config/MySpringMvcDispatcherServletInitializer.java
@@ -0,0 +1,21 @@
+package ru.artyomfadeyev.config;
+
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MySpringMvcDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{SpringConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
