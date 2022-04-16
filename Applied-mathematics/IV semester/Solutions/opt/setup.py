f9c596e02b6ce9bd008b59606285ead69238bf0f
fadyat <fadyat@icloud.com>
2022-04-16 14:48:04 +0300

    move packages to other project

---
+++ Applied-mathematics/IV semester/Solutions/opt/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='opt',
+      version='0.1',
+      description='opt algo\'s',
+      url='https://github.com/fadyat/',
+      author='fadyat',
+      author_email='fadyat@icloud.com',
+      license='MIT',
+      packages=['src'],
+      zip_safe=False)
