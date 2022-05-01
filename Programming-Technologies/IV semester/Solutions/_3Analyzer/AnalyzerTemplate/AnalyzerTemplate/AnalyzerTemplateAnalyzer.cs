198099ad946e73299fe7816367ff62ff8dc2dfcd
fadyat <fadyat@icloud.com>
2022-05-01 13:53:11 +0300

    analyzator for inverting if statement

--- Programming-Technologies/IV semester/Solutions/_3Analyzer/AnalyzerTemplate/AnalyzerTemplate/AnalyzerTemplateAnalyzer.cs
+++ Programming-Technologies/IV semester/Solutions/_3Analyzer/AnalyzerTemplate/AnalyzerTemplate/AnalyzerTemplateAnalyzer.cs
@@ -15,40 +15,48 @@ namespace AnalyzerTemplate
     {
         public const string DiagnosticId = "AnalyzerTemplate";
 
-        // You can change these strings in the Resources.resx file. If you do not want your analyzer to be localize-able, you can use regular strings for Title and MessageFormat.
-        // See https://github.com/dotnet/roslyn/blob/main/docs/analyzers/Localizing%20Analyzers.md for more on localization
         private static readonly LocalizableString Title = new LocalizableResourceString(nameof(Resources.AnalyzerTitle), Resources.ResourceManager, typeof(Resources));
         private static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(Resources.AnalyzerMessageFormat), Resources.ResourceManager, typeof(Resources));
         private static readonly LocalizableString Description = new LocalizableResourceString(nameof(Resources.AnalyzerDescription), Resources.ResourceManager, typeof(Resources));
         private const string Category = "Naming";
 
-        private static readonly DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, isEnabledByDefault: true, description: Description);
+        private static readonly DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Error, isEnabledByDefault: true, description: Description);
 
         public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(Rule); } }
 
         public override void Initialize(AnalysisContext context)
         {
-            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
             context.EnableConcurrentExecution();
-
-            // TODO: Consider registering other actions that act on syntax instead of or in addition to symbols
-            // See https://github.com/dotnet/roslyn/blob/main/docs/analyzers/Analyzer%20Actions%20Semantics.md for more information
-            context.RegisterSymbolAction(AnalyzeSymbol, SymbolKind.NamedType);
-        }
-
-        private static void AnalyzeSymbol(SymbolAnalysisContext context)
-        {
-            // TODO: Replace the following code with your own analysis, generating Diagnostic objects for any issues you find
-            var namedTypeSymbol = (INamedTypeSymbol)context.Symbol;
-
-            // Find just those named type symbols with names containing lowercase letters.
-            if (namedTypeSymbol.Name.ToCharArray().Any(char.IsLower))
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.Analyze | GeneratedCodeAnalysisFlags.ReportDiagnostics);
+            context.RegisterSyntaxNodeAction(c =>
             {
-                // For all such symbols, produce a diagnostic.
-                var diagnostic = Diagnostic.Create(Rule, namedTypeSymbol.Locations[0], namedTypeSymbol.Name);
+                var ifStatement = c.Node as IfStatementSyntax;
+                var elseStatement = ifStatement.Else;
 
-                context.ReportDiagnostic(diagnostic);
-            }
+                if (elseStatement == null) return;
+
+                var block = elseStatement.ChildNodes()
+                                         .OfType<BlockSyntax>()
+                                         .FirstOrDefault();
+
+                if (block == null) return;
+
+                var blockStatements = block.Statements;
+
+                if (blockStatements.Count != 1) return;
+
+                var blockStatement = blockStatements.First();
+                
+                if (!(blockStatement.IsKind(SyntaxKind.ReturnStatement)
+                       || blockStatement.IsKind(SyntaxKind.ThrowStatement)))
+                {
+                    return;
+                }
+                
+                var diagnostics = Diagnostic.Create(Rule, ifStatement.GetLocation(), "Can invert if statement");
+
+                c.ReportDiagnostic(diagnostics);
+            }, SyntaxKind.IfStatement);
         }
     }
 }
---
+++ Programming-Technologies/IV semester/Solutions/_3Analyzer/Roslyn/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Roslyn;
+
+public class Program
+{
+    public static void Main()
+    {
+        var statement = "if_statement_example";
+        if (statement is string)
+        {
+            Console.WriteLine(statement, "it is a string!");
+        }
+        else
+        {
+            throw new Exception(statement);
+        }
+    }
+}
