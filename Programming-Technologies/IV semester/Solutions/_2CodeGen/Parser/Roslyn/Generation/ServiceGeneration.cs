50c144186a5a874a314571844464987c0261a825
fadyat <fadyat@icloud.com>
2022-05-06 00:59:21 +0300

    add client field

--- Programming-Technologies/IV semester/Solutions/_2CodeGen/Parser/Roslyn/Generation/ServiceGeneration.cs
+++ Programming-Technologies/IV semester/Solutions/_2CodeGen/Parser/Roslyn/Generation/ServiceGeneration.cs
@@ -1,35 +1,59 @@
+using System.Collections.Immutable;
 using AntlrParser.Analysis;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace Roslyn.Generation;
 
 public static class ServiceGeneration
 {
-    public static string Generate(IEnumerable<MethodDeclaration> methodsDeclaration, string serviceName)
+    public static string Generate(ImmutableList<MethodDeclaration> methodsDeclaration, string serviceName)
     {
         var namespaceDeclaration = SyntaxFactory.NamespaceDeclaration(
             SyntaxFactory.ParseName($"{serviceName}Generated")
         );
 
+        var clientValue = SyntaxFactory.EqualsValueClause(
+            SyntaxFactory.ObjectCreationExpression(
+                SyntaxFactory.Token(SyntaxKind.NewKeyword),
+                SyntaxFactory.ParseTypeName("HttpClient"),
+                SyntaxFactory.ArgumentList(),
+                null
+            ));
+        
+        var clientField = SyntaxFactory.FieldDeclaration(SyntaxFactory.VariableDeclaration(
+            SyntaxFactory.ParseTypeName("HttpClient"),
+            SyntaxFactory.SeparatedList(new[]
+            {
+                SyntaxFactory.VariableDeclarator(
+                    SyntaxFactory.Identifier("client"), 
+                    null,
+                    clientValue
+                )
+            })))
+            .AddModifiers(SyntaxFactory.Token(SyntaxKind.PrivateKeyword));
+
         var classDeclaration = SyntaxFactory.ClassDeclaration(serviceName)
-            .AddModifiers(SyntaxFactory.Token(SyntaxKind.PublicKeyword));
+            .AddModifiers(SyntaxFactory.Token(SyntaxKind.PublicKeyword))
+            .AddMembers(clientField);
 
 
-        foreach (var method in methodsDeclaration)
+        methodsDeclaration.ForEach(method =>
         {
             var methodBody = SyntaxFactory.ParseStatement(
-                $"return HttpClient().{method.HttpMethodName}(\"{method.Url}\");"
+                $"return await client.{method.HttpMethodName}Async(\"{method.Url}\");"
             );
 
             var methodDeclaration = SyntaxFactory.MethodDeclaration(
                     SyntaxFactory.ParseTypeName(method.ReturnType), method.MethodName)
                 .AddModifiers(SyntaxFactory.Token(SyntaxKind.PublicKeyword))
                 .WithBody(SyntaxFactory.Block(methodBody))
+                .WithParameterList(SyntaxFactory.ParameterList())
                 .NormalizeWhitespace();
 
-            Console.WriteLine(methodDeclaration);
-        }
+            classDeclaration = classDeclaration.AddMembers(methodDeclaration);
+        });
 
         namespaceDeclaration = namespaceDeclaration.AddMembers(classDeclaration)
             .NormalizeWhitespace();
