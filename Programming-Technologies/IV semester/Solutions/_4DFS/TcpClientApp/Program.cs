6dddedb58957ababa2719b66ab54783a645e83a5
fadyat <fadyat@icloud.com>
2022-05-08 12:56:31 +0300

    Simple TCP-server and TCP-client interaction

---
+++ Programming-Technologies/IV semester/Solutions/_4DFS/TcpClientApp/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+namespace TcpClientApp;
+
+internal static class Program
+{
+    private const int Port = 8888;
+    private const string ServerIp = "127.0.0.1";
+
+    private static void Main()
+    {
+        try
+        {
+            var server = new TcpClient();
+            server.Connect(ServerIp, Port);
+
+            var data = new byte[256];
+            var response = new StringBuilder();
+            var stream = server.GetStream();
+
+            do
+            {
+                var bytes = stream.Read(data, 0, data.Length);
+                response.Append(Encoding.UTF8.GetString(data, 0, bytes));
+            } while (stream.DataAvailable);
+
+            Console.WriteLine(response.ToString());
+
+            stream.Close();
+            server.Close();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Exception: {e.Message}");
+        }
+
+        Console.WriteLine("Request completed...");
+    }
+}
---
+++ Programming-Technologies/IV semester/Solutions/_4DFS/TcpServerApp/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace TcpServerApp;
+
+public static class Program
+{
+    private static void Main()
+    {
+        TcpListener? server = null;
+        const int port = 8888;
+        try
+        {
+            var serverIp = IPAddress.Parse("127.0.0.1");
+            server = new TcpListener(serverIp, port);
+            server.Start();
+
+            while (true)
+            {
+                Console.WriteLine("Waiting for connections... ");
+                
+                var client = server.AcceptTcpClient();
+                Console.WriteLine("Client connected. Completing a request...");
+
+                var stream = client.GetStream();
+                const string response = "Hello world!";
+                var data = Encoding.UTF8.GetBytes(response);
+
+                stream.Write(data, 0, data.Length);
+                Console.WriteLine($"Message sent: {response}\n");
+                
+                stream.Close();
+                client.Close();
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+        finally
+        {
+            server?.Stop();
+        }
+    }
+}
